c0ee8b996aaf2055526f2952cc64730b57241c95
suhaila ramadan <suhailaramadan@gmail.com>
2024-08-31 16:06:52 +0300

    splash

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp( 
+      debugShowCheckedModeBanner: false,
+      routes: {
+        
+      },
+    );
+  }
+}
